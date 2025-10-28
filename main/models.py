c2e7cfd1f5d5c79abc2e673f473da07e46cb2f1b
rellbgs23 <farrelbagas23@gmail.com>
2025-10-28 22:41:48 +0700

    fix desc not showing ahahha

--- main/models.py
+++ main/models.py
@@ -17,8 +17,11 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.URLField()
-    category = models.CharField()
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES
+    )
     is_featured = models.BooleanField()
 
     def __str__(self):
-        return self.id
+        return self.name
